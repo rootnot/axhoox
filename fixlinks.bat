f596e2d5f75b06376a19154fc7cb6776c52c91a5
Rafał Grabie <rootnot@o2.pl>
2013-06-21 20:36:26 +0200

    still symlinks issues ...

--- fixlinks.bat
+++ fixlinks.bat
@@ -1,1 +1,2 @@
-mklink /H generated-prototype\__js\axhoox.js source\axhoox.js
+IF EXIST generated-prototype\__js\axhoox.js del generated-prototype\__js\axhoox.js
+mklink generated-prototype\__js\axhoox.js ..\..\source\axhoox.js
