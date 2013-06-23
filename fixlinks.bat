9ee336e2217dd8bfd02717ff15ef2ca7256d281f
Rafał Grabie <rootnot@o2.pl>
2013-06-23 16:18:14 +0200

    after rebase fixes

--- fixlinks.bat
+++ fixlinks.bat
@@ -1,2 +1,4 @@
-IF EXIST generated-prototype\__js\axhoox.js del generated-prototype\__js\axhoox.js
-mklink /H generated-prototype\__js\axhoox.js source\axhoox.js
+IF EXIST generated-prototype\__axhoox\axhoox.js del generated-prototype\__axhoox\axhoox.js
+mklink /H generated-prototype\__axhoox\axhoox.js source\axhoox.js
+IF EXIST generated-prototype\__axhoox\axhooxload.html del generated-prototype\__axhoox\axhooxload.html
+mklink /H generated-prototype\__axhoox\axhooxload.html source\axhooxload.html
