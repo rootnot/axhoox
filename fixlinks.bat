bf8dcd4f1c657b219a7c310263a21d93fe0e8a68
Rafał Grabie <rootnot@o2.pl>
2013-07-09 20:55:07 +0200

    small fix

--- fixlinks.bat
+++ fixlinks.bat
@@ -1,3 +1,4 @@
+@echo off
 IF EXIST generated-prototype\__axhoox\axhoox.js del generated-prototype\__axhoox\axhoox.js
 mklink /H generated-prototype\__axhoox\axhoox.js source\axhoox.js
 IF EXIST generated-prototype\__axhoox\axhooxload.html del generated-prototype\__axhoox\axhooxload.html
