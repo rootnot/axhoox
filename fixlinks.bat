ce28aedadf614ebebe0c82ccb1133b8239eafe4e
Rafał Grabie <rootnot@o2.pl>
2013-07-11 19:09:05 +0200

    Description to fixlinks added.

--- fixlinks.bat
+++ fixlinks.bat
@@ -1,4 +1,16 @@
 @echo off
+
+goto START
+
+This is for development and testing.
+Symbolic links used in windows systems are not properly recognized by GIT so there is a problem with
+linking source files to their locations in a testing prototype.
+
+IMPORTANT!
+
+You should use this command every time GIT updates (changes) source files.
+
+:START
 IF EXIST generated-prototype\__axhoox\axhoox.js del generated-prototype\__axhoox\axhoox.js
 mklink /H generated-prototype\__axhoox\axhoox.js source\axhoox.js
 IF EXIST generated-prototype\__axhoox\axhooxload.html del generated-prototype\__axhoox\axhooxload.html
