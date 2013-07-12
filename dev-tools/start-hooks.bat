842d0a80231fde496ab585e4fa48e2a0be77af1b
Rafał Grabie <rootnot@o2.pl>
2013-07-12 23:57:08 +0200

    added maintenance scripts

---
+++ dev-tools/start-hooks.bat
@@ -0,0 +1,29 @@
+@echo off
+
+goto :START
+
+This is for development and testing.
+This script activates post-checkout hook script.
+
+:INSTALL
+rem Linking subroutine
+
+IF EXIST "%~dpf2" del "%~dpf2"
+
+mklink /H "%~dpf2" "%~dpf1" > nul
+
+echo Done, post-checkout hook installed to %~dpf2
+
+goto :EOF
+
+
+:START
+rem Main action
+
+set HOOK_FILE=post-checkout
+
+call :INSTALL %~p0\%HOOK_FILE% %~p0..\.git\hooks\%HOOK_FILE%
+
+IF errorlevel 1 (
+	echo Some tasks went wrong. Runtime errors occured.
+)
